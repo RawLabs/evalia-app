9962728812aa12b1907bcffe59a9d5ffdff5e407
Raw Labs <maltesecanuk@protonmail.com>
2025-08-18 15:37:11 -0600

    Update logging config to create static directory on Streamlit Cloud

--- core/logging_config.py
+++ core/logging_config.py
@@ -1,36 +1,22 @@
-"""Configure logging for Evalia."""
 import logging
-import logging.handlers
 import os
-import streamlit as st
+from logging.handlers import RotatingFileHandler
 
 def configure_evalia_logger():
-    if st.session_state.get("_evalia_logger_configured"):
-        return logging.getLogger("EvaliaLogger")
-
-    logger = logging.getLogger("EvaliaLogger")
+    logger = logging.getLogger("evalia")
     logger.setLevel(logging.DEBUG)
-    logger.propagate = False
 
-    for h in list(logger.handlers):
+    # Remove existing handlers to avoid duplication
+    for h in logger.handlers[:]:
         logger.removeHandler(h)
 
     log_file = "static/evalia_debug.log"
-    fh = logging.handlers.RotatingFileHandler(log_file, maxBytes=5 * 1024 * 1024, backupCount=3)
+    # Create static directory if it doesn't exist
+    os.makedirs(os.path.dirname(log_file) or '.', exist_ok=True)
+    fh = RotatingFileHandler(log_file, maxBytes=5 * 1024 * 1024, backupCount=5)
     fh.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s - [File: %(pathname)s, Line: %(lineno)d]")
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
-    if os.getenv("ENV") != "STREAMLIT_CLOUD":
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
-
-    for noisy in ("urllib3", "httpx", "PIL", "requests"):
-        logging.getLogger(noisy).setLevel(logging.WARNING)
-
-    st.session_state["_evalia_logger_configured"] = True
-    logger.debug("Evalia logger configured.")
     return logger
